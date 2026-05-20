edacffac18a6ce0644b6ad89f622d6c19c6ede66
clindta <clindta@gmail.com>
2026-05-20 20:46:22 +0200

    connect supabase

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 // ====================================================================
 
 window.GF_CONFIG = {
-  SUPABASE_URL: 'YOUR_SUPABASE_URL_HERE',
-  SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY_HERE',
+  SUPABASE_URL: 'https://ocwpktikmqfevbolbbii.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jd3BrdGlrbXFmZXZib2xiYmlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyOTMyMTAsImV4cCI6MjA5NDg2OTIxMH0.0BsYFI_TDMN4Z6BID2b0sobkizLCshHJFR0EKWEQjSc',
   CONTACT_EMAIL: 'clindta@gmail.com',
   OWNER_FIRST_NAME: 'Chris'
 };
